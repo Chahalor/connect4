c66a01e46b4c9d322354f394c8871902d167e06a
nduvoid <nduvoid@student.42mulhouse.fr>
2025-05-20 15:15:03 +0200

    add: core: check for win

--- src/Core/_internal/_core.c
+++ src/Core/_internal/_core.c
@@ -51,7 +51,7 @@ __attribute__((always_inline, used)) static inline _t_Core	*_Init(
 }
 
 /** */
-__attribute__((always_inline, used)) static inline int	_core_destroy(
+__attribute__((always_inline, used)) static inline int	_Destroy(
 	_t_Core *Core
 )
 {
@@ -62,12 +62,62 @@ __attribute__((always_inline, used)) static inline int	_core_destroy(
 }
 
 /** */
+__attribute__((always_inline, used)) static inline int	count_in_direction(
+	const _t_Core *const restrict _Core,
+	t_point point,
+	const int dx,
+	const int dy,
+	const char pawn
+)
+{
+	register int	count = 0;
+
+	while (
+		   point.x >= 0
+		&& point.x < _Core->height
+		&& point.y >= 0
+		&& point.y < _Core->height
+		&& _Core->grid[point.y][point.x] == pawn)
+	{
+		++count;
+		point.x += dx;
+		point.y += dy;
+	}
+	return (count);
+}
+
+/** */
+__attribute__((always_inline, used)) static inline int	is_winning_move(
+	const int x,
+	const int y,
+	const _t_Core *_Core
+)
+{
+	static const int	directions[4][2] = {{1, 0}, {0, 1}, {1, 1}, {1, -1}};
+	const char			pawn = _Core->grid[y][x];
+	register int		i = 0;
+
+	while (i < 4)
+	{
+		int	dx = directions[i][0];
+		int	dy = directions[i][1];
+		int	count = 1;
+		count += count_in_direction(_Core, (t_point){x + dx, y + dy}, dx, dy, pawn);
+		count += count_in_direction(_Core, (t_point){x - dx, y - dy}, -dx, -dy, pawn);
+		if (count > 3)
+			return (1);
+	}
+	return (0);
+}
+
+
+/** */
 __attribute__((always_inline, used)) static inline int	_core_add_pown(
 	_t_Core *const restrict Core,
 	register const t_uint col
 )
 {
-	register t_uint			x = 0;
+	register t_uint	x = 0;
 
 	if (_unlikely(!Core))
 		return (core_ord_stop);
@@ -82,6 +132,8 @@ __attribute__((always_inline, used)) static inline int	_core_add_pown(
 		if (x == Core->height)
 			return (-2);
 		Core->grid[x][col] = Core->turn ? AI_PAWN : PLAYER_PAWN;
+		if (is_winning_move(col, x, Core))
+			return (Core->turn ? core_ord_win_AI : core_ord_win_player);
 		Core->turn = !Core->turn;
 		--Core->nb_case_left;
 		return (1);
@@ -113,7 +165,7 @@ __attribute__((hot, visibility("hidden"))) int	_Core(
 			return (-!(Core != NULL));
 		}
 		case core_req_destroy:
-			return (_core_destroy(Core));
+			return (_Destroy(Core));
 		default:
 			return (-1);
 	}
