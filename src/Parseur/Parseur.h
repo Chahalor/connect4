8234dc8fdafd0ebfcfb7f4e1ddcda0169f3e0fdb
Henri Trapp <htrapp@k1r3p11.42mulhouse.fr>
2025-05-20 15:26:26 +0200

    fix:error-struct-parserv4

--- src/Parseur/Parseur.h
+++ src/Parseur/Parseur.h
@@ -10,6 +10,7 @@ typedef struct s_config
 	int	columns;
 	int	rows;
 	unsigned int	interface_enabled   : 1;
+	unsigned int	error         : 1;
 }	t_config;
 
 t_config	check_args(int argc, char **argv);
