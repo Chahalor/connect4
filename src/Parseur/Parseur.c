16e769fd9a688501956c0a3087907715ca53ce6d
Henri Trapp <htrapp@k1r3p11.42mulhouse.fr>
2025-05-20 16:49:42 +0200

    Fix:double-affichage-erreur

--- src/Parseur/Parseur.c
+++ src/Parseur/Parseur.c
@@ -49,20 +49,20 @@ t_config	check_args(int argc, char **argv)
 	int	rows;
 
 	if (argc != 3 && argc != 4)
-		print_error_and_exit();
+		return (print_error_and_exit());
 	if (!is_numeric(argv[1]) || !is_numeric(argv[2]))
-		print_error_and_exit();
+		return (print_error_and_exit());
 	cols = ft_atoi(argv[1]);
 	rows = ft_atoi(argv[2]);
 	if (cols < 6 || rows < 7)
-		print_error_and_exit();
+		return (print_error_and_exit());
 	config.columns = cols;
 	config.rows = rows;
 	config.interface_enabled = 0;
 	if (argc == 4)
 	{
 		if (!(argv[3][0] == '0' || argv[3][0] == '1') || argv[3][1] != '\0')
-			print_error_and_exit();
+			return (print_error_and_exit());
 		config.interface_enabled = (argv[3][0] == '1');
 	}
 	return (config);
